01a2ebd3d222e2c94276136ad04b33a48350c242
Artemis Macadam <441macadamia@gmail.com>
2024-01-07 19:14:36 +0300

    что-то пошло не так и в прошлом коммите я залил половину интернета...

--- lesson_23/cypress/e2e/shopCart.spec.cy.js
+++ lesson_23/cypress/e2e/shopCart.spec.cy.js
@@ -1,18 +1,17 @@
-describe('Поиск товара на 5element', () => {
-  it('Поиск товара и проверка результатов', () => {
-    const searchQuery = 'iphone';
 
-    cy.visit('https://5element.by');
-    cy.get('.js-cookie-popup-close', { timeout: 10000 }).should('be.visible');
-    cy.get('.js-cookie-popup-close').click();
 
-    cy.get('.js-mm-opener').click();
-    cy.get('[data-trig="drop-2"] > .catalog-navigation-trig').click();
-    cy.get('#filters-0 > .filter-body > :nth-child(1) > :nth-child(2)').click();
-    cy.get(':nth-child(14) > .item-box > .item-box-link').click();
-    cy.get(':nth-child(1) > :nth-child(1) > .card-product-full > :nth-child(3) > .c-controls > .btn').click();
-    cy.get('[href="/cart"]').click();
-    cy.get('.section-product-body').should('exist')
+const homePage = require('../../pages/homePage');
 
-  });
+describe('Shop cart on 5 element', () => {
+  
+    it('Shop cart should exist after adding products into it', () => {
+
+
+      homePage.visitHomePage('https://5element.by');
+      homePage.openProductsCatalog();
+      homePage.selectBrand();
+      homePage.addProductToCart();
+      homePage.goToCart();
+      homePage.isCartNotEmpty();
+    });
 });
---
+++ lesson_23/cypress/e2e/shopCartClear.spec.cy.js
@@ -0,0 +1,19 @@
+
+const homePage = require('../../pages/homePage');
+
+describe('Shop cart clear on 5 element', () => {
+  it('Shop cart should not exist after deleting products from it', () => {
+
+    homePage.visitHomePage('https://5element.by');
+    homePage.openProductsCatalog();
+    homePage.selectBrand();
+    homePage.addProductToCart();
+    homePage.goToCart();
+    homePage.removeProductFromCart();
+    homePage.isCartEmpty();
+
+
+
+
+  });
+});
